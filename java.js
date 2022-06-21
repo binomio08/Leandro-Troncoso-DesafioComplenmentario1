db9269b3724d182c43fce338cb75b57e84fdbfc0
Leandro Troncoso <binomio08@hotmail.com>
2022-06-20 23:56:52 -0300

    Segundo Commit

---
+++ java.js
@@ -0,0 +1,5 @@
+let objetos = "";
+for(let i = 0; i < 5; i++){
+    objetos += prompt("ingresas 5 objetos indispensables con los que sale a la calle") +"\n";
+}
+alert("No Salga sin estos objetos: "+ "\n" + objetos);
